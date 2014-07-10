486dec6566962a58fc6ffc4bc516d6f6de02dac6
Miguel Santiago <gadaxara@gmail.com>
2014-07-10 09:28:46 +0200

    Initial Commit

---
+++ HeroLocator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HeroLocator
+//
+//  Created by Miguel Santiago Rodríguez on 10/07/14.
+//  Copyright (c) 2014 gadaxara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HeroLocator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HeroLocator
+//
+//  Created by Miguel Santiago Rodríguez on 10/07/14.
+//  Copyright (c) 2014 gadaxara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
